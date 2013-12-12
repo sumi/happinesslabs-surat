f1dbd6036a24410fd3fa4a2879692090fb019ff0
ec2-user@ip-10-212-97-41 <ec2-user@happinesslabs.com>
2013-12-12 10:24:03 +0000

    commited all changed files from html folder to git repo

---
+++ ffmpeg/test.php
@@ -0,0 +1,47 @@
+<?php
+error_reporting(0);
+if($_POST['make_video_name'] == 'make video')
+{
+	$ffpeg_path = 'C:/ffmpeg/bin/';
+	$path = 'C:/wamp/www/ffmpeg';
+	$cmd = $ffpeg_path.'ffmpeg -r 1/2 -i '.$path.'/images/img%03d.jpg -c:v libx264 -vf "fps=25,format=yuv420p" '.$path.'/output/out.mp4 2>&1';
+	exec($cmd,$output,$return);
+	
+	if($return==0)
+	{
+		$msg = "video created successfully...";
+	}
+	else
+	{
+		$msg = "video already exist";
+	}
+}?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+</head>
+<body>
+<table width="200" border="1">
+  <tr>
+    <td>image1</td>
+    <td>image2</td>
+  </tr>
+  <tr>
+    <td><img src="images/img000.jpg" name="img000" width="100" height="100"/></td>
+    <td><img src="images/img001.jpg" name="img001" width="100" height="100"/></td>
+  </tr>  
+</table>
+<form method="post" name="make_form">
+<input type="submit" name="make_video_name" value="make video" id="make_video_btn"/>
+</form>
+<div class="msg"><?php echo $msg;?></div>
+<div class="show_video">
+<video width="320" height="240" controls autoplay>
+  <source src="output/out.mp4" type="video/mp4" id="video">
+</video>
+</div>
+
+</body>
+</html>
