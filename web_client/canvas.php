68514234e0bd5eae0292e5801d62b2a2c943c1d1
ec2-user@ip-10-212-97-41 <ec2-user@happinesslabs.com>
2013-12-13 10:32:25 +0000

    commited all changed files from html folder to git repo

--- web_client/canvas.php
+++ web_client/canvas.php
@@ -10,6 +10,8 @@
 <body>
 <canvas id="myCanvas" width="400" height="400" style="border:1px solid #000000;"></canvas>
 <img src="images/1.jpg" id="my-image"/>
+<img width="42" height="44" style="cursor:pointer" onclick="changeImage('background_career_1.png')" alt="" src="images/1.jpg" />
+
 <script type="text/javascript">
 //START INITIALIZE CANVAS INSTANCE
 var canvas=new fabric.Canvas('myCanvas',{
