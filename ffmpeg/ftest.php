6b0dcd205c433f61dbf6198ac998fb73cbc8a7d6
ec2-user@ip-10-212-97-41 <ec2-user@happinesslabs.com>
2013-12-14 10:00:07 +0000

    commited all changed files from html folder to git repo

---
+++ ffmpeg/ftest.php
@@ -0,0 +1,64 @@
+<?php
+error_reporting(0);
+if($_POST['make_video_name'] == 'make video')
+{
+	//exec("mogrify -resize 800x600 images/*.JPG");
+	//die;
+	$videoFile = rand().'_out';
+	$cmdvid = 'ffmpeg -f image2 -r 1/5 -i images/img%03d.jpg -r 25 output/'.$videoFile.'.mp4 2>&1';
+	//$cmdvid = "ffmpeg -f image2 -i images/img%03d.jpg -t 10s -r 25 output/output.mp4 2>&1";
+	//$cmdvid = "ffmpeg -r 1/5 -f image2 -i 'images/img%03d.jpg' -r 25 output/".$videoFile.".mp4 2>&1";
+	exec($cmdvid,$outvid,$returnvid);
+	
+	//$cmd1 = "ffmpeg  -i output/".$videoFile.".mpg output/".$videoFile.".mp4  2>&1";
+	//exec($cmd1,$out1,$return1);
+	
+		
+	//$cmd = "ffmpeg -i ".$videoFile." -c:v libx264 -ar 22050 -crf 28 output/out.flv 2>&1";
+	
+	//$cmd = 'ffmpeg -i images/img%03d.jpg output/out.mp4 2>&1';
+	//exec($cmd,$output,$return);
+	echo $cmdvid;
+	echo "<br>";
+	echo $cmd1;
+	echo '<pre>';
+	print_r($outvid);
+	echo '</pre>';
+	//echo "======";
+	echo '<pre>';
+	//print_r($out1);
+	echo '</pre>';
+	
+	if($returnvid==0)
+		$msg = "video created successfully...";
+}?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+</head>
+<body>
+<h1>ffmpeg</h1>
+<table width="200" border="1">
+  <tr>
+    <td>image1</td>
+    <td>image2</td>
+  </tr>
+  <tr>
+    <td><img src="images/img001.png" name="img001" width="100" height="100"/></td>
+    <td><img src="images/img002.png" name="img002" width="100" height="100"/></td>
+  </tr>  
+</table>
+<form method="post" name="make_form">
+<input type="submit" name="make_video_name" value="make video" id="make_video_btn"/>
+</form>
+<div class="msg"><?php echo $msg;?></div>
+<div class="show_video">
+<video width="320" height="240" controls autoplay>
+  <source src="output/<?php echo $videoFile.'.mp4' ;?>" type="video/mp4" id="video">
+</video>
+</div>
+
+</body>
+</html>
--- web_client/canvas.php
+++ web_client/canvas.php
@@ -2,18 +2,107 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Canvas</title>
+<title>Canvas Image Upload</title>
+<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
 <script type="text/javascript" src="js/jquery.min.js"></script>
 <script type="text/javascript" src="js/jquery-ui-1.8.16.custom.min.js"></script>
 <script type="text/javascript" src="js/fabric.js"></script>
+<style>
+body {
+	padding-top: 40px;
+}
+#main {
+	margin-top: 80px;
+}
+</style>
 </head>
 <body>
-<canvas id="myCanvas" width="400" height="400" style="border:1px solid #000000;">
-<img id="photo_img" src="images/1.jpg" class="canvas-img">
+<header>
+  <div class="topbar">
+    <div class="fill">
+      <div class="container"><a class="brand" href="canvas.php">Happyness labs - Caanvas</a></div>
+    </div>
+  </div>
+</header>
+<div id="main" class="container">
+<form method="post">
+<canvas id="myCanvas" width="256px" height="256px" style="border:1px solid #000000; float:right;">
+<img id="photo_img" src="images/1.jpg" class="canvas-img" />
 </canvas>
+<br/>
 <img width="50" height="50" style="cursor:pointer" onclick="changeImage('1.jpg')" alt="" src="images/1.jpg" />
 <img width="50" height="50" style="cursor:pointer" onclick="changeImage('2.jpg')" alt="" src="images/2.jpg" />
 <img width="50" height="50" style="cursor:pointer" onclick="changeImage('3.jpg')" alt="" src="images/3.jpg" />
+<img width="50" height="50" style="cursor:pointer" onclick="changeImage('4.jpg')" alt="" src="images/4.jpg" />
+<img width="50" height="50" style="cursor:pointer" onclick="changeImage('5.jpg')" alt="" src="images/5.jpg" />
+<img width="50" height="50" style="cursor:pointer" onclick="changeImage('6.jpg')" alt="" src="images/6.jpg" />
+<img width="50" height="50" style="cursor:pointer" onclick="changeImage('7.jpg')" alt="" src="images/7.jpg" />
+<img width="50" height="50" style="cursor:pointer" onclick="changeImage('8.jpg')" alt="" src="images/8.jpg" />
+<br/>
+<img width="42" height="44" style="cursor:pointer" onclick="addClipart('clipart_love_1.png')" alt="" src="images/expertboard/background/clipart_love_1.png">
+<img width="42" height="44" style="cursor:pointer" onclick="addClipart('clipart_love_2.png')" alt="" src="images/expertboard/background/clipart_love_2.png">
+<img width="42" height="44" style="cursor:pointer" onclick="addClipart('clipart_love_3.png')" alt="" src="images/expertboard/background/clipart_love_3.png">
+<img width="42" height="44" style="cursor:pointer" onclick="addClipart('clipart_love_4.png')" alt="" src="images/expertboard/background/clipart_love_4.png">
+<img width="42" height="44" style="cursor:pointer" onclick="addClipart('clipart_love_5.png')" alt="" src="images/expertboard/background/clipart_love_5.png">
+<img width="42" height="44" style="cursor:pointer" onclick="addClipart('clipart_love_6.png')" alt="" src="images/expertboard/background/clipart_love_6.png">
+<img width="42" height="44" style="cursor:pointer" onclick="addClipart('clipart_love_7.png')" alt="" src="images/expertboard/background/clipart_love_7.png">
+<img width="42" height="44" style="cursor:pointer" onclick="addClipart('clipart_love_8.png')" alt="" src="images/expertboard/background/clipart_love_8.png">
+<br/>
+<div class="clearfix">
+<label>Add Text in Canvas</label>
+<div class="input">
+<textarea class="right_Upload_input_box" name="txtcomment" id="txtcomment">Enter your text.</textarea>
+</div>
+</div>
+<br/>
+<div class="clearfix">
+<label>Add Text in Canvas</label>
+<div class="input">
+<select onchange="changeTextFont(this.value)" name="font_type" class="right_Upload_select" id="font_type">
+        <option value="Arial" selected="selected">Arial</option>
+        <option value="Times New Roman">Times NR</option>
+        <option value="Verdana">Verdana</option>
+        <option value="Courier New">Courier</option>
+        </select>
+        </div>
+</div>
+<br/>
+<div class="clearfix">
+<label>Add Text in Canvas</label>
+<div class="input">
+<select onchange="changeTextFontSize(this.value)" name="font_size" id="font_size" class="right_Upload_select">
+        <option value="14" selected="selected">14px</option>
+        <option value="16">16px</option>
+        <option value="18">18px</option>
+        <option value="24">24px</option>
+        <option value="30">30px</option>
+        <option value="45">45px</option>
+        </select>
+        </div>
+</div>
+<br/>
+<div class="clearfix">
+<label>Add Text in Canvas</label>
+<div class="input">
+<select class="right_Upload_select" onchange="changeTextFontColor(this.value)" name="font_color" id="font_color">
+			<option value="white">White</option>
+            <option value="black">Black</option>
+            <option value="red">Red</option>
+            <option value="green">Green</option>
+            </select>
+            </div>
+</div>
+<br/>
+<button onclick="return addTextCanvas();" class="btn primary">Add Text</button>
+<br/><br/>
+<button onclick="return saveCanvas();"  class="btn primary">Save Canvas</button>
+<br/><br/>
+<button onclick="return editCanvas();" class="btn primary">Edit Canvas</button>
+<br/><br/>
+<input type="file" name="uploadfile" id="uploadimg" onclick="fileupload();" />
+<div id="previewjpeg"></div>
+<br/><br/>
+<button id="remove_image" title="Delete" onclick="return removeText();" style="display: none;" class="btn primary">Delete Text or ClipArt</button>
    <input type="hidden" value="new_exp_photo" name="photoType" id="photoType" />
     <input type="hidden" value="1" name="pedit_photo_day" id="pedit_photo_day" />
     <input type="hidden" value="0" name="file_name" id="file_name" />
@@ -23,11 +112,39 @@
     <input type="hidden" name="jsonVal" id="jsonVal" />
 	<input type="hidden" name="category" id="category" value="love"/>
 	<input type="hidden" name="imgType" id="imgType" value="background"/>
-
+</form>
 <script type="text/javascript">
+//START INITIALIZE CANVAS INSTANCE
+var canvas=new fabric.Canvas('myCanvas',{
+  hoverCursor:'pointer',
+  selection:false
+});
+var imgElement=document.getElementById('photo_img');
+var imgInstance=new fabric.Image(imgElement,{
+  left: 128,
+  top: 128,
+  opacity: 1
+});
+canvas.add(imgInstance);
+<!-- ADD CLIPART IN CANVAS -->
+function addClipart(imgPhoto){
+	fabric.Image.fromURL('images/expertboard/background/'+imgPhoto,function(img){
+		img.set({
+		  left: fabric.util.getRandomInt(50,190),
+		  top: fabric.util.getRandomInt(50,190)
+		});
+		img.perPixelTargetFind=true;
+		img.targetFindTolerance=4;
+		img.hasControls=img.hasBorders=true;
+		img.on('selected',function(){
+		  document.getElementById('remove_image').style.display='block';
+		});
+		canvas.add(img).setActiveObject(img);
+    });	 
+}
 //START FUNCTION CHANGE IMAGE
 function changeImage(file_name){
-	//canvas.clear().renderAll();
+	canvas.clear().renderAll();
 	document.getElementById("photo_img").src='images/'+file_name;
 	document.getElementById('file_name').value=file_name;
 	document.getElementById("load_dir").value='background/';
@@ -37,11 +154,11 @@ function changeImage(file_name){
 	});	
 	fabric.Image.fromURL('images/'+file_name,function(img){
 		img.set({
-		  left:97,
-		  top:97,
-		  opacity: 0.85,
-		  height:192,
-		  width:192
+		  left:128,
+		  top:128,
+		  opacity: 1,
+		  height:256,
+		  width:256
 		});
 		img.perPixelTargetFind=false;
 		img.targetFindTolerance=4;
@@ -52,6 +169,146 @@ function changeImage(file_name){
 		canvas.calcOffset();
     });	
 }
+//START ADD TEXT IN CANVAS FUNCTION
+function addTextCanvas(){
+	var txtComment=document.getElementById('txtcomment').value;
+	var addText=new fabric.Text(txtComment, {
+		left: 97,
+		top: 97,
+		fontSize: 14,
+		fill: 'white',
+		fontFamily:'arial'
+	});
+	addText.on('selected',function(){
+	  document.getElementById('remove_image').style.display='block';
+	});
+	canvas.add(addText).setActiveObject(addText);
+	return false;
+}
+function changeTextFontColor(font_color){
+	var txtComment=document.getElementById('txtcomment').value;
+	canvas.getActiveObject().set("fill",font_color);
+	canvas.renderAll();  	
+}
+function changeTextFont(font_name){
+	var txtComment=document.getElementById('txtcomment').value;
+	canvas.getActiveObject().set("fontFamily",font_name);
+	canvas.renderAll();
+}
+function changeTextFontSize(text_size){
+	var txtComment=document.getElementById('txtcomment').value;
+	canvas.getActiveObject().set("fontSize",text_size);
+	canvas.renderAll();
+}
+function removeText(){
+	if(confirm('Are you sure to delete Clipart/Text?')){
+	   canvas.remove(canvas.getActiveObject());
+	   canvas.renderAll();
+	   document.getElementById('remove_image').style.display='none';
+	}
+	return false;
+}
+//START SAVE CANVAS FUNCTION
+function saveCanvas(){	
+	canvas.deactivateAllWithDispatch().renderAll();
+	var json=JSON.stringify(canvas);
+	document.getElementById('jsonVal').value=json;	
+	var dataURL=canvas.toDataURL();	
+	canvas.clear().renderAll();
+	fabric.Image.fromURL(dataURL,function(img){
+		img.set({
+		  left:128,
+		  top:128,
+		  opacity: 1,
+		  height:256,
+		  width:256
+		});
+		img.perPixelTargetFind=false;
+		img.targetFindTolerance=4;
+		img.evented=img.selectable=false;
+		img.hasControls=img.hasBorders=false;
+		canvas.add(img);
+    });
+	return false;
+}
+//START EDIT CANVAS FUNCTION
+function editCanvas(){
+	var json=document.getElementById('jsonVal').value;
+	canvas.loadFromJSON(json);
+	canvas.renderAll();
+	canvas.calcOffset();
+	var dataURL=canvas.toDataURL();
+	canvas.clear().renderAll();
+	fabric.Image.fromURL(dataURL,function(img){
+		img.set({
+		  left:128,
+		  top:128,
+		  opacity: 1,
+		  height:256,
+		  width:256
+		});
+		img.perPixelTargetFind=true;
+		img.targetFindTolerance=4;
+		img.evented=img.selectable=true;
+		img.hasControls=img.hasBorders=true;
+		canvas.add(img);
+    });
+	return false;
+}
 </script>
+<script type="text/javascript">
+function fileupload()
+{
+	$('#uploadimg').live("change", function()
+	{  
+		var files=$('#files');
+		var oFReader = new FileReader();
+		oFReader.readAsDataURL(this.files[0]);
+		console.log(this.files[0]);
+		oFReader.onload = function (oFREvent) 
+		{           var results = oFREvent.target.result;
+					results=results.replace(/(\r\n|\n|\r)/gm,"");
+					results_array=results.split('##===##');
+					var fname=results_array[0];
+					var upload_file=results_array[1];
+					document.getElementById('file_name').value=fname;
+					document.getElementById('load_dir').value='temp/';
+					/*document.getElementById("photo_img").src=upload_file;
+					canvas.clear().renderAll();*/
+					fabric.Image.fromURL(upload_file,function(img){
+						img.set({
+						  left: fabric.util.getRandomInt(50,190),
+						  top: fabric.util.getRandomInt(50,190),
+						  height:100,
+						  width:100
+						});
+						img.perPixelTargetFind=true;
+						img.targetFindTolerance=4;
+						img.hasControls=img.hasBorders=true;
+						img.on('selected',function(){
+						  document.getElementById('remove_image').style.display='block';
+						});
+						canvas.add(img).setActiveObject(img);
+					});	
+					canvas.on('object:selected',function(){
+					  document.getElementById('remove_image').style.display='none';
+					});
+					canvas.renderAll(); 	
+					canvas.calcOffset();
+					//START UNLINK TEMP DIRECTORY IMAGE
+					$.ajax({
+						type: "POST",
+						url: "save_canvas_image.php",
+						data: {path: upload_file}
+					}).done(function(respond){
+						//alert(respond);
+					});
+				
+			      $('#previewjpeg').html('<img src="'+oFREvent.target.result+'">');
+		};
+	}); 
+}
+</script>
+</div>
 </body>
 </html>
